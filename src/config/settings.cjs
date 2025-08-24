38b765d69866dfb65326c64ef585e1ec123b29f1
AJIN <niroshanbot@gmail.com>
2025-08-24 14:33:30 +0530

    Create settings.cjs

---
+++ src/config/settings.cjs
@@ -0,0 +1,38 @@
+module.exports = {
+  "PREFIX": ".",
+  "OWNERS": [
+    "94762526630@s.whatsapp.net"
+  ],
+  "PORT": "3000",
+  "AUTOREADSTATUS": true,
+  "MODE": "public",
+  "BOTNUMBER": "94762526630",
+  "OMDBAPI_KEY": "76cb7f39",
+  "READCMD": true,
+  "AUTOVOICE": false,
+  "AUTOSTICKER": true,
+  "AUTOREPLY": true,
+  "AUTOREACT": false,
+  "WELCOME": false,
+  "ANTIBAD": true,
+  "ANTIBOT": true,
+  "ANTILINK": false,
+  "ANTICALL": false,
+  "ANTIDELETE": true,
+  "ALLWAYSONLINE": true,
+  "MOROCCOBLOCK": true,
+  "OWNERNAME": "MR YASHMIKA",
+  "NEWSJID": "120363393420637374@g.us",
+  "MOVIEJID": false,
+  "ANIMEJID": "120363367650594508@g.us",
+  "AUTOTYPING": false,
+  "AUTORECORDING": true,
+  "AUTONEWS": false,
+  "OWNERREACT": false,
+  "EMOJI": "🏷",
+  "AIMODE": false,
+  "BANNED": [],
+  "SUDO": [
+    "94702572157"
+  ]
+};
